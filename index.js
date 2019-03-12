5411cd5eaad8e91861d0a0ecaa33d7a66f635c61
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-12 00:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // Code your solution in this file!
-const headquatersBlock = 42
+```const headquartersBlock = 42
+const feetPerBlock = 264
 
+// Code your solution in this file!
 function distanceFromHqInBlocks(block) {
-  return Math.abs(block - headquatersBlock)
+  return Math.abs(block - headquartersBlock)
 }
 
-function distanceFromHqInFeet(block) { return distanceFromHqInBlocks(block) * feetPerBlock
+function distanceFromHqInFeet(block) {
+  // return Math.abs(block - headquartersBlock) * feetPerBlock
+  return distanceFromHqInBlocks(block) * feetPerBlock
 }
 
 function distanceTravelledInFeet(block1, block2) {
-  return Math.abs((block1 * feetPerBlock) - (blocks2 * feetPerBlock))
+  // 43, 48
+  // 43 -> 11352
+  // 48 -> 12672
+  return Math.abs((block1 * feetPerBlock) - (block2 * feetPerBlock))
+  return Math.abs(block2 - block1) * feetPerBlock
 }
 
 function calculatesFarePrice(start, destination) {
-  let feetTraveled = distanceTravelledInFeet(start, destination) if (feetTraveled < 400) { 
-  return 0
-  } else if (feetTraveled >= 400 && feetTraveled <= 2000 ) 
-  {return (feetTraveled - 400) * 0.02} else if { (feetTraveled > 2000) { return 25} else {
+  let feetTraveled = distanceTravelledInFeet(start, destination)
+  // 43, 44
+  if (feetTraveled < 400) {
+    return 0
+  } else if (feetTraveled >= 400 && feetTraveled <= 2000) {
+    return (feetTraveled - 400) * 0.02
+  } else if (feetTraveled > 2000 && feetTraveled <= 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
